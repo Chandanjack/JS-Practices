10f4aa8f299c8a054e00ca5d7512e95bc0c391a7
Chandan Kumar <101467768+Chandanjack@users.noreply.github.com>
2024-07-28 12:42:32 +0000

    Datatype

---
+++ 01_Basics/Datatype.js
@@ -0,0 +1,30 @@
+"use strict" ; // treat all JS code as newer version
+
+// alert(3 + 3) //we are using nodejs, not browser
+
+console.log( 3 
+     + 3) // code readablility should be high
+
+console.log("Chandan")  
+
+let name = "Chandan"
+let age = 24
+let isLoggedId = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "Chandan")
+console.log(typeof 25)
+cconsole.log(typeof false)
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
