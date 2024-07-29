0df98b7418d5740ad06862d57e87071a161d1d9b
Chandan Kumar <101467768+Chandanjack@users.noreply.github.com>
2024-07-29 19:13:00 +0000

    DataType-Summary

---
+++ 01_Basics/05_DataType-summary.js
@@ -0,0 +1,38 @@
+// Primitive DataType---------
+
+//  7 type : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 3438743302295379720203n
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["Shaktiman", "Naagraj", "Doga"];
+let myObj ={
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("hello");
+}
+
+console.log(typeof bigNumber);
+
+console.log(typeof scoreValue);
+
+console.log(typeof myFunction);
