c6390ebe836b1af45f5c0ed963fb36cf5fcacf56
Chandan Kumar <101467768+Chandanjack@users.noreply.github.com>
2024-07-29 11:20:21 +0000

    conversionOpration

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+// let score = "33ac"
+// let score = null
+// let score = undefined
+let score = true
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber) 
+
+//"33" =>33
+//"33ac" => NaN
+// true =>; false => 0
+
+//-----------------------------
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//-----------------------------
+
+let someNumber =33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
