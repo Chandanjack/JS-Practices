d2b1a3f7c75972b299787ebdc866d93d3bd41ddb
Chandan Kumar <101467768+Chandanjack@users.noreply.github.com>
2024-07-30 12:17:56 +0000

    intro

---
+++ 02_advance/01_hello.js
@@ -0,0 +1,24 @@
+console.log("hello world!")
+
+//--------------------------------------------
+// Single line comment .it is similar to comments in c++
+
+/*
+This is a multiline comment in JavaScript
+it is very similar to comment in programming
+*/
+//----------------------------------------------
+
+function sayHello(){
+    alert("Hello world!")
+}
+
+// var use
+
+var myVar = "global"; // Declare aglobe variable
+function checkscope(){
+    var myVar = "local"; //Declare alocal variable
+    console.log(myVar);
+
+}
+
