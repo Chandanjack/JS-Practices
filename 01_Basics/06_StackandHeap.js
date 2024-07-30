a44f206878547e0da7fb3063a81eac0b66cb7649
Chandan Kumar <101467768+Chandanjack@users.noreply.github.com>
2024-07-30 09:21:15 +0000

    Stack And Heap

---
+++ 01_Basics/06_StackandHeap.js
@@ -0,0 +1,22 @@
+// Stack (primitive), heap (Non-Primitive)
+
+let myYoutubename = "Chandankumarsingh"
+
+let anothername =myYoutubename
+anothername = "theeasysmile"
+
+console.log(myYoutubename)
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ypl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "chandanksingh2022@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
